98babc6dcad31273b95ab198067412bd0ab2f778
Rihab Naouar <rihabbennaouar@gmail.com>
2024-08-21 17:02:24 +0100

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './UserList.css';
+
+function UserList() {
+    const [listOfUsers, setListOfUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then(response => {
+                console.log("This is the response from the link:", response);
+                setListOfUsers(response.data);
+            })
+            .catch(error => {
+                console.log("The error is:", error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <ul className="user-list">
+                {listOfUsers.map(user => (
+                    <li key={user.id} className="user-item">
+                        <h3><span>The name is :</span> {user.name}</h3><hr className='line  '/>
+                        <p className="address"><span className="address-label">The adress is : </span><br/>
+                        {user.address.street}, {user.address.suite},{user.address.city}</p>
+                    </li>   
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default UserList;
